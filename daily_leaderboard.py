4c47305f17c4ec033d72befa176aaf0255089829
Yifan Lin <132865370+EvaLin2951@users.noreply.github.com>
2023-12-16 22:17:39 -0500

    Add files via upload

---
+++ daily_leaderboard.py
@@ -0,0 +1,26 @@
+import datetime
+import Account
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+
+def leaderboard(date):
+    date = datetime.now().strftime("%Y-%m-%d")
+    leaderboard = "Coin Leaderboard (" + date + "):\n\n"
+
+    sorted_accounts = Account.coin_accounts.find().sort('coin_balance', -1)
+    order = 1
+    for account in sorted_accounts:
+        leaderboard += str(order) + ". {}: {}\n".format(account['email'], account['coin_balance'])
+        order += 1
+    leaderboard += "\n\n🎉 🎉 🎉"
+
+    return leaderboard
+
+def send(token, channel, message):
+    client = WebClient(token=token)
+
+    try:
+        client.chat_postMessage(channel=channel, text=message)
+        print(f"Message sent to channel {channel}")
+    except SlackApiError as e:
+        print(f"Error sending message: {e}")
