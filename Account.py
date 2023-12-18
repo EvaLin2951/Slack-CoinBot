596dc84d75277539f44f39184c9f17d383080339
Yifan Lin <132865370+EvaLin2951@users.noreply.github.com>
2023-12-18 00:12:12 -0500

    Update Account.py

--- Account.py
+++ Account.py
@@ -23,14 +23,14 @@ def bind_account(slack_user_id, email):
     # Check if an account with the same email already exists
     query = {'email': email}
     result = coin_accounts.find_one(query)
-    created_at = int(datetime.now().timestamp())
     if result is None:
         # If no account exists with the email, create a new one
+        created_at = int(datetime.now().timestamp())
         new_account = Account(slack_user_id, email, 100, created_at, created_at)
         coin_accounts.insert_one(new_account.__dict__)
     else:
         # If an account with the same email exists, bind it to the slack_user_id
-        coin_accounts.update_one(query, {'$set': {'slack_user_id': slack_user_id, 'created_at': created_at, 'updated_at': created_at}})
+        coin_accounts.update_one(query, {'$set': {'slack_user_id': slack_user_id}})
     return "Account binding successful"
 
 def get_balance(slack_user_id):
@@ -42,7 +42,7 @@ def get_balance(slack_user_id):
     account_balance = coin_accounts.find_one({'slack_user_id': slack_user_id})['coin_balance']
     return "Your coin balance is " + str(account_balance)
 
-def transfer_coin(slack_user_id, to_email, amount):
+def transfer_coin(slack_user_id, email, to_email, amount):
     from_query = {'slack_user_id': slack_user_id}
     to_query = {'email': to_email}
     
@@ -50,26 +50,25 @@ def transfer_coin(slack_user_id, to_email, amount):
     if coin_accounts.count_documents(from_query) == 0:
         return "Your account has not been bound yet"
     
-    # If an account with the same slack_user_id exists, check if the target account exists
+    # Check if the target account exists
     if coin_accounts.count_documents(to_query) == 0:
         return "The target account does not exist"
     
-    # If both coin_accounts exist, check if the account's balance is sufficient
-    account_balance = coin_accounts.find_one(from_query)['coin_balance']
-    if amount > int(account_balance):
-        return "Insufficient coin balance"
-    to_account_balance = coin_accounts.find_one(to_query)['coin_balance']
-    
-    # If the account's balance is sufficient, transfer the coin
-    account_balance -= amount
-    to_account_balance += amount
-    
-    updated_at = int(datetime.now().timestamp())
-    coin_accounts.update_one(from_query, {'$set': {'coin_balance': account_balance, 'updated_at': updated_at}})
-    coin_accounts.update_one(to_query, {'$set': {'coin_balance': to_account_balance, 'updated_at': updated_at}})
-    coin_flows.insert_one({'from_slack_user_id': slack_user_id, 'to_email': to_email, 'amount': amount, 'date_time': updated_at})
-    
-    return "Coin transfer successful, you now have " + str(account_balance) + " coins"
+    # If both coin_accounts exist, transfer the coin if the account's balance is sufficient
+    with client.start_session() as session:
+        with session.start_transaction():
+
+            updated_at = int(datetime.now().timestamp())
+            from_query['coin_balance'] = {'$gte': amount}
+            result = coin_accounts.update_one(from_query, {'$inc': {'coin_balance': -amount}, '$set': {'updated_at': updated_at}})
+            if result.modified_count == 0:
+                return "Insufficient coin balance"
+            coin_accounts.update_one(to_query, {'$inc': {'coin_balance': amount}, '$set': {'updated_at': updated_at}})
+            
+            coin_flows.insert_one({'email': email, 'to_email': to_email, 'change_amount': -amount, 'date_time': updated_at})
+            coin_flows.insert_one({'email': to_email, 'from_email': email, 'change_amount': amount, 'date_time': updated_at})
+
+    return "Coin transfer successful, you now have " + str(coin_accounts.find_one(from_query)['coin_balance']) + " coins"
 
 def admin_add_coin(slack_user_id, to_email, amount):
     admin_query = {'slack_user_id': slack_user_id, 'is_admin': True}
@@ -84,11 +83,9 @@ def admin_add_coin(slack_user_id, to_email, amount):
         return "The target account does not exist"
     
     # If both coin_accounts exist, transfer the coin
-    to_account_balance = coin_accounts.find_one(to_query)['coin_balance']
-    to_account_balance += amount
-
     updated_at = int(datetime.now().timestamp())
-    coin_accounts.update_one(to_query, {'$set': {'coin_balance': to_account_balance, 'updated_at': updated_at}})
-    coin_flows.insert_one({'from_slack_user_id': "", 'to_email': to_email, 'amount': amount, 'date_time': updated_at})
+    coin_accounts.update_one(to_query, {'$inc': {'coin_balance': amount}, '$set': {'updated_at': updated_at}})
+    
+    coin_flows.insert_one({'email': to_email, 'change_amount': amount, 'date_time': updated_at})
 
     return "Coin added successfully"
