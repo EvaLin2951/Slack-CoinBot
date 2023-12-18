a1a5cdaf10d5367ec539e544d8359fe1accf14fa
Yifan Lin <132865370+EvaLin2951@users.noreply.github.com>
2023-12-18 00:11:40 -0500

    Update app.py

--- app.py
+++ app.py
@@ -35,6 +35,7 @@ def handle_command_transfer(ack, say, client, command):
     ack()
 
     user_id = command['user_id']
+    user_email = client.users_info(user=user_id)['user']['profile']['email']
     transfer_info = command['text'].split()
     if len(transfer_info) != 2:
         say("Invalid transfer format. Please use /transfer <to_email> <amount>")
@@ -42,10 +43,9 @@ def handle_command_transfer(ack, say, client, command):
     to_email = transfer_info[0]
     amount = int(transfer_info[1])
     
-    say(f"{Account.transfer_coin(user_id, to_email, amount)}")
+    say(f"{Account.transfer_coin(user_id, user_email, to_email, amount)}")
 
-    user_email = client.users_info(user=user_id)['user']['profile']['email']
-    to_id = Account.coin_accounts.find_one({'email': to_email})["slack_user_id"]
+    to_id = Account.coin_accounts.find_one({'email': to_email}).get("slack_user_id")
     if to_id:
         message = "You have received " + str(amount) + " coins from " + user_email + ", you now have " + str(Account.coin_accounts.find_one({'email': to_email})["coin_balance"]) + " coins"
         client.chat_postMessage(channel=to_id, text=message)
@@ -64,7 +64,7 @@ def handle_command_add(ack, say, client, command):
 
     say(f"{Account.admin_add_coin(user_id, to_email, amount)}")
 
-    to_id = Account.coin_accounts.find_one({'email': to_email})["slack_user_id"]
+    to_id = Account.coin_accounts.find_one({'email': to_email}).get("slack_user_id")
     if to_id:
         message = "You have received " + str(amount) + " coins, you now have " + str(Account.coin_accounts.find_one({'email': to_email})["coin_balance"]) + " coins"
         client.chat_postMessage(channel=to_id, text=message)
